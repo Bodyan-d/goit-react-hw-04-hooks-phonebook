bd0c0763c7de7fa99bd387889f780741fddc1423
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-07-29 21:47:53 +0300

    made-CL-function

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,27 +1,19 @@
-import { React, Component } from 'react';
+import React from 'react';
 import './ContactList.css';
 
-export default class ContactList extends Component {
-  render() {
-    const { contacts, deleteItem } = this.props;
-
-    return (
-      <ul>
-        {contacts.map(contact => {
-          return (
-            <li key={contact.id} className="list-item">
-              {`${contact.name}: ${contact.number}`}
-              <button
-                id={contact.id}
-                onClick={deleteItem}
-                className="delete-btn"
-              >
-                delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
+export default function ContactList({ contacts, deleteItem }) {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id} className="list-item">
+            {`${contact.name}: ${contact.number}`}
+            <button id={contact.id} onClick={deleteItem} className="delete-btn">
+              delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
